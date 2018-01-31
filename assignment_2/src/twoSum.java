a79a3067b5a96dc6f79357942a3865cd2e74db0b
Sherlock.Z <542447275@qq.com>
2018-01-31 14:01:47 -0800

    Update twoSum.java

--- assignment_2/src/twoSum.java
+++ assignment_2/src/twoSum.java
@@ -6,7 +6,7 @@ public class twoSum {
     public static void main(String args[]){
         int[] nums = {1,3, 2, 7, 11, 15};
         int target = 9;
-        int[] res = twoSum3(nums, target);
+        int[] res = twoSum2(nums, target);
         for(int a : res)
             System.out.print(a + " ");
     }
